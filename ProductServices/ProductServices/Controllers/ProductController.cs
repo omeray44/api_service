05548243d7ea1f5ac46bee34606f85b87bb93491
Omer AYDOGAN <omeraydoganis@hotmail.com>
2019-12-05 23:45:22 +0300

    api
    api product services

---
+++ ProductServices/ProductServices/Controllers/ProductController.cs
@@ -0,0 +1,47 @@
+﻿using ProductDataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web.Http;
+
+namespace ProductServices.Controllers
+{
+    public class ProductController : ApiController
+    {
+        public IEnumerable<Products> Get()
+        {
+            using (ProductEntities entities = new ProductEntities())
+            {
+                return entities.Products.ToList();
+            }
+        }
+
+        public Products Get(int id)
+        {
+            using (ProductEntities entities = new ProductEntities())
+            {
+                return entities.Products.FirstOrDefault(p => p.ID == id);
+            }
+        }
+
+        //public static byte[] Hash(string id, byte[] salt)
+        //{
+        //    return Hash(Encoding.UTF8.GetBytes(id), salt);
+        //}
+
+        //public static byte[] Hash(byte[] value, byte[] salt)
+        //{
+        //    byte[] saltedValue = value.Concat(salt).ToArray();
+        //    // Alternatively use CopyTo.
+        //    //var saltedValue = new byte[value.Length + salt.Length];
+        //    //value.CopyTo(saltedValue, 0);
+        //    //salt.CopyTo(saltedValue, value.Length);
+
+        //    return new SHA256Managed().ComputeHash(saltedValue);
+        //}
+    }
+}
